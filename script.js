402dd09f6af474873e9f680fba3a052b7cd51ce3
tebibux <tebibusolomon79@gmail.com>
2022-09-24 23:55:18 +0300

    Operation Started

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-const previousOperation = document.querySelector('.previousOperation')
-previousOperation.contains = ''
+class Calculator {
+	constructor(previousOperation, newOperation) {
+		this.previousOperation = previousOperation;
+		this.newOperation = newOperation;
+		this.clearAll();
+	}
+	clearAll() {
+		this.previousOperation = '';
+		this.newOperation = '';
+		this.calculate = undefined;
+	}
+	deleteOperand() {
 
-// class Calculator{
-// 	constructor()
-// }
+	}
+	appendOperand(value) {
+	}
+	calculate() {
+
+	}
+
+	display(value) {
+		this.previousOperation = value;
+		console.log(this.previousOperation)
+	}
+};
+
+const newOperation = document.querySelector('.newOperation');
+const previousOperation = document.querySelector('.previousOperation');
+const clear = document.getElementsByClassName('allClear-delete');
+const operation = document.querySelector('.op');
+const numbers = document.querySelectorAll('.number');
+
+// instantiation here
+const calculator = new Calculator(previousOperation, newOperation);
+
+
+numbers.forEach(number => {
+	number.addEventListener('click', () => {
+		// console.log(number.innerText);
+		calculator.display(number.innerText);
+	}
+	)
+});
