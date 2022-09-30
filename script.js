63d4e2a8545c69fe04af9d3140180ee198565498
tebibux <tebibusolomon79@gmail.com>
2022-10-01 01:45:54 +0300

    number parsing done

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Constant Variable declaration
+// display boxes
+const currentBox = document.querySelector('.currentBox');
+const previousBox = document.querySelector('.previousBox');
 // Used to retrieve all the operation buttons
 const operationBtn = document.querySelectorAll('.operation');
 // Used to retrieve all the delete buttons
@@ -6,13 +9,56 @@ const deleteBtn = document.querySelectorAll('.btnDelete');
 // Used to retrieve all the number buttons
 const numberBtn = document.querySelectorAll('.numberBtn');
 
-
-// console.log(currentNumber.join(''));
-// let i = currentNumber.join('');
-// console.log(parseInt(i))
+// variable result is used to store result after multiplication
+let result;
+// temp variable for number to store clicked number
+let num = '';
 numberBtn.forEach(number => {
 	number.addEventListener('click', (e) => {
-		console.log(e)
+		if (e.target.innerText === '.' && num.includes('.')) return;
+
+		else {
+			if (e.target.innerText === '.' && currentBox.innerText === '') {
+				num = 0;
+			};
+			num += e.target.innerText;
+			currentBox.innerText = num;
+		}
+
 	})
 });
+// temp variable for storing an operation
+let operation = '';
+operationBtn.forEach(opCommand => {
+	opCommand.addEventListener('click', (e) => {
 
+		if ((e.target.innerText === '*' && operation.includes('*')) ||
+			(e.target.innerText === '/' && operation.includes('/')) ||
+			(e.target.innerText === '+' && operation.includes('+')) ||
+			(e.target.innerText === '-' && operation.includes('-'))
+		) return;
+		else {
+			operation = e.target.innerText;
+			previousBox.innerText += num;
+			previousBox.innerText += operation;
+			num = '';
+			calculate(operation);
+			operation = '';
+		}
+	})
+});
+// function to calculate the operation
+function calculate(command) {
+	let firstNumber = '';
+	let secondNumber = '';
+	secondNumber = currentBox.innerText;
+	firstNumber = previousBox.innerText;
+	firstNumber.slice(0, -1);
+	secondNumber.slice(0, -1);
+	console.log(secondNumber);
+	if(command === '*'){
+		result = parseFloat(firstNumber) * parseFloat(secondNumber);
+		currentBox.innerText = '';
+		currentBox.innerText = result;
+	}
+}
