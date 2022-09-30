8fb67222d95247454b6244e8497d78ae582ac724
tebibux <tebibusolomon79@gmail.com>
2022-09-30 21:01:28 +0300

    second round

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Constant Variable declaration
+// Used to retrieve all the operation buttons
+const operationBtn = document.querySelectorAll('.operation');
+// Used to retrieve all the delete buttons
+const deleteBtn = document.querySelectorAll('.btnDelete');
+// Used to retrieve all the number buttons
+const numberBtn = document.querySelectorAll('.numberBtn');
+
+
+// console.log(currentNumber.join(''));
+// let i = currentNumber.join('');
+// console.log(parseInt(i))
+numberBtn.forEach(number => {
+	number.addEventListener('click', (e) => {
+		console.log(e)
+	})
+});
+
